ca81821628cd959d6d68c94c3936ae942fdc31f4
Agata Domingues <124289385+htadmg@users.noreply.github.com>
2023-10-31 13:58:26 -0300

    Add project files.

---
+++ MyClassesTest/FileProcessTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MyClassesTest
+{
+    [TestClass]
+    public class FileProcessTest
+    {
+        [TestMethod]
+        public void FileNameDoesExists()
+        {
+
+            Assert.Inconclusive();
+        }
+        [TestMethod]
+        public void FileNameDoesNotExists()
+        {
+
+            Assert.Inconclusive();
+        }
+        [TestMethod]
+        public void FileNameNullOrEmpty_ThorwsArgumentNullException()
+        {
+
+            Assert.Inconclusive();
+        }
+    }
+}
---
+++ MyClassesTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyClassesTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyClassesTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1e15b0b8-2361-4d8b-b74f-919379a6f1c3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
