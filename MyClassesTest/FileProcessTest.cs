4cd342543916c204fe2db8238d105839bf0aef53
Agata Domingues <124289385+htadmg@users.noreply.github.com>
2023-11-06 09:45:24 -0300

    att

--- MyClassesTest/FileProcessTest.cs
+++ MyClassesTest/FileProcessTest.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyClasses;
 using System;
 
 namespace MyClassesTest
@@ -9,14 +10,24 @@ namespace MyClassesTest
         [TestMethod]
         public void FileNameDoesExists()
         {
+            FileProcess fp = new FileProcess();
 
-            Assert.Inconclusive();
+            bool fromCall;
+
+            fromCall = fp.FileExists(@"C:\Users\Agata Farias\Desktop\Smartbility\bin\Debug\net5.0\Smartbility.exe");
+
+            Assert.IsTrue(fromCall);
         }
         [TestMethod]
         public void FileNameDoesNotExists()
         {
+            FileProcess fp = new FileProcess();
 
-            Assert.Inconclusive();
+            bool fromCall;
+
+            fromCall = fp.FileExists(@"C:\Users\Desktop\Smartbility\bin\Debug\net5.0\Smartbility.exe");
+
+            Assert.IsTrue(fromCall);
         }
         [TestMethod]
         public void FileNameNullOrEmpty_ThorwsArgumentNullException()
